7a9be6b8014f9275efa00a4c877884afa3bcd541
Marcelo Gusmão Gumiero <mgumiero9@gmail.com>
2017-10-22 12:16:59 -0200

    Adds ViewPager layout visibility control.

--- app/src/main/java/com/mgumiero9/firebasecommunication/DemoFragment.java
+++ app/src/main/java/com/mgumiero9/firebasecommunication/DemoFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
@@ -23,9 +24,21 @@ public class DemoFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         int position = FragmentPagerItem.getPosition(getArguments());
         TextView title = view.findViewById(R.id.item_title);
+        LinearLayout llInit = view.findViewById(R.id.demo_ll_init_page);
+        LinearLayout llData = view.findViewById(R.id.demo_ll_data);
+
         title.setText(String.valueOf(position));
 
-
+        setupViewPagerLayoutVisibility(position, llData, llInit);
     }
 
+    private void setupViewPagerLayoutVisibility(int position, LinearLayout llData, LinearLayout llInit) {
+        if (position > 0) {
+            llData.setVisibility(View.VISIBLE);
+            llInit.setVisibility(View.GONE);
+        } else {
+            llData.setVisibility(View.GONE);
+            llInit.setVisibility(View.VISIBLE);
+        }
+    }
 }
